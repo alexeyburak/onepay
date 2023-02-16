3dd3ff9e0a2b74afca07ba207336eb733f249d4f
Alexey Burak <byraklesha@gmail.com>
2023-02-16 10:49:22 +0300

    feat: add user crud operations

--- user/src/main/java/com/burak/user/controller/UserController.java
+++ user/src/main/java/com/burak/user/controller/UserController.java
@@ -1,6 +1,14 @@
 package com.burak.user.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.burak.user.dto.UserDTO;
+import com.burak.user.model.User;
+import com.burak.user.service.impl.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * onepay
@@ -9,5 +17,42 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserServiceImpl userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
+        User user = userService.getUserById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addNewUser(@RequestBody UserDTO user) {
+        userService.addNewUser(user);
+        return new ResponseEntity<>("Add new user", HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") Long id) {
+        userService.deleteById(id);
+        return new ResponseEntity<>("Delete user", HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable("id") Long id,
+                                             @RequestBody UserDTO user) {
+        userService.updateById(id, user);
+        return new ResponseEntity<>("Update user", HttpStatus.OK);
+    }
+
 }
