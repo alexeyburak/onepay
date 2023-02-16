3a47e59f241affabda1bf29ee4f66ec32b2013b6
Alexey Burak <byraklesha@gmail.com>
2023-02-16 11:00:20 +0300

    feat: add exception handler

---
+++ user/src/main/java/com/burak/user/exception/ApiExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.burak.user.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+/**
+ * onepay
+ * Created by Alexey Burak
+ * Feb 2023
+ */
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(value = {ApiRequestException.class})
+    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e) {
+        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+
+        ApiException apiException = new ApiException(
+                e.getMessage(),
+                e,
+                badRequest,
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
+        return new ResponseEntity<>(apiException, badRequest);
+    }
+}
