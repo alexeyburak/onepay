0bf8550d2e0f3de993ca2a4f4a672e55494365c1
Alexey Burak <byraklesha@gmail.com>
2023-02-16 10:48:54 +0300

    feat: add user dto

---
+++ user/src/main/java/com/burak/user/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package com.burak.user.dto;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * onepay
+ * Created by Alexey Burak
+ * Feb 2023
+ */
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserDTO {
+    String name;
+    String password;
+}
