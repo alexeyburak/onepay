981158e628f9bcce59550541940be465bd4a99bf
Alexey Burak <byraklesha@gmail.com>
2023-02-22 12:55:23 +0300

    feat: add operation history dto

---
+++ balance/src/main/java/com/burak/balance/dto/OperationHistoryDTO.java
@@ -0,0 +1,29 @@
+package com.burak.balance.dto;
+
+import com.burak.balance.model.enums.OperationStatus;
+import com.burak.balance.model.enums.OperationType;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ * onepay
+ * Created by Alexey Burak
+ * Feb 2023
+ */
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OperationHistoryDTO {
+    BigDecimal sum;
+    LocalDate dateCreatedAt;
+    LocalTime timeCreatedAt;
+    OperationType type;
+    OperationStatus status;
+}
