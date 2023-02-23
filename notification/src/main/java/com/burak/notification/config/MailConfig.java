e749ac031f08dca93a864c892645921884e469c4
Alexey Burak <byraklesha@gmail.com>
2023-02-23 20:11:40 +0300

    feat: add mail configuration

--- notification/src/main/java/com/burak/notification/config/MailConfig.java
+++ notification/src/main/java/com/burak/notification/config/MailConfig.java
@@ -1,10 +1,50 @@
 package com.burak.notification.config;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
 /**
  * onepay
  * Created by Alexey Burak
  * Feb 2023
  */
 
+@Configuration
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class MailConfig {
+    @Value("${spring.mail.host}")
+    String host;
+    @Value("${spring.mail.username}")
+    String username;
+    @Value("${spring.mail.password}")
+    String password;
+    @Value("${spring.mail.port}")
+    int port;
+    @Value("${spring.mail.protocol}")
+    String protocol;
+    @Value("${mail.debug}")
+    String debug;
+
+    @Bean
+    public JavaMailSender getMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties properties = mailSender.getJavaMailProperties();
+
+        properties.setProperty("mail.transport.protocol", protocol);
+        properties.setProperty("mail.debug", debug);
+        return mailSender;
+    }
 }
